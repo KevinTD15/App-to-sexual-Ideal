ae62a96788de0d182ef7bf21168fcd3d6e010990
KevinTD15 <talaveradiazkevin1999@gmail.com>
2022-12-07 23:53:47 -0500

    arreglados parametros para optimizacion

--- modelo.py
+++ modelo.py
@@ -1,12 +1,24 @@
 from posturas import *
 from restricciones import *
 import scipy.optimize as sp
+import matplotlib.pyplot as plt
 
-def EjecutarModelo(minimizar, criterio, umbral_org, energia_ini, placer_ini, listRestricciones):
+def EjecutarModelo(minimizar, criterio, umbral_org, energia_ini, placer_ini, listRestricciones, fo):
     placer_posturas = [x.placer for x in posturas]
     agotamiento_posturas = [x.agotamiento for x in posturas]
     restricciones = CrearRestricciones(listRestricciones, placer_posturas, agotamiento_posturas)
-    resultado = Modelo(minimizar, energia_ini, umbral_org, restricciones, criterio)
+    resultado = Modelo(minimizar, energia_ini, umbral_org, restricciones, fo, placer_ini)
+    
+    fig, ax = plt.subplots()
+    #temperaturas = {'Placer':placer, 'Cansancio':cansancio}
+    #ax.plot(placer_posturas, resultado.x, label = 'Placer')
+    p = [x.nombre for x in posturas]
+    plt.bar(p, resultado.x)
+    ax.legend(loc = 'best')
+    plt.show()
+    #ax.plot(p, resultado.x, label = 'Placer')
+    #ax.legend(loc = 'best')
+    #plt.show()
     
     return resultado
 
@@ -19,15 +31,19 @@ def CrearRestricciones(listRestricciones, placer_posturas, agotamiento_posturas)
             restr.append((placer_posturas, 1))
     return restr
 
-def Modelo(minimizar, ei, uo, restr, crit):
+def Modelo(minimizar, ei, uo, restr, crit, pi):
     list = []
     for i in restr:
         if i[1] == 1:
             list.append( [(-1 * item) for item in i[0]])
+            uo = -1 * uo
         else:
             list.append(i[0])
 
     a_ub_1 = list
+    #a_ub_1.append([0, -1, 0])
+    #a_ub_1.append([-1, 0, 0])
+    #a_ub_1.append([0, 0, -1])
 
     fo = []
     if(minimizar == True):
@@ -35,7 +51,13 @@ def Modelo(minimizar, ei, uo, restr, crit):
     else:
         fo = [(-1 *int(item)) for item in crit]
 
-    b_ub = [ei, uo]
+    b_ub = [ei, pi + uo]
+    
+    bounds1 = (1, None)
+    bounds2 = (1, None)
+    bounds3 = (1, None)
+    bounds4 = (1, None)
+    bounds5 = (1, None)
     
 
-    return sp.linprog(c=fo, A_ub = a_ub_1, b_ub = b_ub ,bounds=(0, None))
+    return sp.linprog(c=fo, A_ub = a_ub_1, b_ub = b_ub , bounds=[bounds1, bounds2, bounds3, bounds4, bounds5])
