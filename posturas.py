bd35605acd347aa6f0e4c0b7af9f0e7d777d026e
KevinTD15 <talaveradiazkevin1999@gmail.com>
2022-12-07 21:42:51 -0500

    creadas posturas

---
+++ posturas.py
@@ -0,0 +1,11 @@
+class Postura: #en talla
+    def __init__(self,nombre):
+        self.nombre = nombre
+        self.placer = None
+        self.agotamiento = None 
+
+p1 = Postura('misionero')
+p2 = Postura('perrito')
+p3 = Postura('69')
+
+posturas = [p1, p2, p3]
