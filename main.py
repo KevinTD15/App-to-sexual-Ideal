a4e813e8b06cb9175d741ab47eb983c61e9bf666
KevinTD15 <talaveradiazkevin1999@gmail.com>
2022-12-11 10:03:03 -0500

    agregado visual

--- main.py
+++ main.py
@@ -1,100 +1,153 @@
-import numpy as np
-import numpy as np
+from tkinter import *
 from posturas import *
-from modelo import *
+from modelo import EjecutarModelo
+import matplotlib.pyplot as plt 
 
-minimizar = False
-criterio = None
-umbral_org = 0
-energia_ini = 0
+class Raiz():
+    def __init__(self):       
+        #crear ventana principal
+        self.ventana = Tk()
+        self.ventana.title("App-to sexual ideal")
+        self.ventana.geometry("400x500"+"+"+str(10)+"+"+str(10)) 
+        self.ventana.resizable(1,1)
+        self.ventana.config(bg="lightblue",relief="sunken",bd=12) 
+        #Label(text="Proyecto Final de PMA  2022\n\n Kevin Talavera Díaz C-311",bg="lightblue",fg="black",font=("arial",12),border=10,justify=CENTER).place(x=220,y=450)
+        Label (text="Ingrese los siguientes datos",bg="lightblue",fg="black",font=("arial",12)).place(x=70,y=20)
 
-# def read_script(minimizar, umbral_org, energia_ini): #poner criterio
-#     with open ('script.txt') as f :
-#         lines = f.readlines()
-#     if(lines[0] == 'minimizar\n'):
-#         minimizar = True
-#     fo = lines[1].split(' ')
-#     list_placer = lines[2].split(' ')
-#     for i in range(len(list_placer)):
-#         posturas[i].placer = list_placer[i]
-#     list_agot = lines[3].split(' ')
-#     for i in range(len(list_agot)):
-#         posturas[i].agotamiento = list_agot[i]
-                    
-#     energia_ini = int(lines[4])
-#     umbral_org = int(lines[5])
-#     list_restr = lines[6].split(' ')
-#     criterio = lines[7]
-#     return minimizar, energia_ini, umbral_org, list_restr, criterio, fo
+        self.canvas = None
+        self.vr = None
+        self.boton = None 
+        self.minimizar = None
+        self.umbral_org = None
+        self.energia_ini = None
+        self.placer_ini = None
+        self.canvasPosturas = None
+        self.posturas = None
+        self.TodosVivos = None
+        self.TodosOrgasmo = None
 
-# var_decision = {}
-# var_decision.update(tiempo_posicion = 't')
-# var_decision.update(cuan_duro = 'cd')
+        self.datosEntrada()
 
-def main():
-    fin = ''
-    while fin != '1':
-        #minimizar = False
-        #criterio = None
-        #umbral_org = 0
-        #energia_ini = 0
-        print('''Las posturas son:
-              1- Misionero
-              2- Perrito
-              3- 69
-              ''')
-        print('''Desea:
-              1- Maximizar
-              2- Minimizar''')
-        minimizar = input() == '2'
-        print('''Criterio:
-              1- Tiempo''')
-        criterio = input()   
-        print('Teclee el umbral de orgasmo')
-        umbral_org = int(input())  
-        print('Teclee la energia inicial')
-        energia_ini = int(input())
-        print('Teclee el placer inicial')
-        placer_ini = int(input())
-        print(f'Tecle los {len(posturas)} valores de la funcion objetivo')
-        fo = [int(x) for x in input().split(' ')]
-        for i in posturas:
-            print(f'Teclee cuanto placer da la postura {i.nombre}')
-            i.placer = int(input()) 
-        for i in posturas:
-            print(f'Teclee cuanto cansa da la postura {i.nombre}')
-            i.agotamiento = int(input()) 
-        print('''Restricciones que desea usar (insertar en la consola por espacios):
-              a- TodosVivos
-              b- TodosOrgasmo''')  
-        listRestricciones = input().split(' ')
-        resultado = EjecutarModelo(minimizar, criterio, umbral_org, energia_ini, placer_ini, listRestricciones, fo)
-        
-        for i in range(len(resultado.x)):
-            print(f'El tiempo que hay que dedicarle a la postura {posturas[i].nombre} es {resultado.x[i]}')
+        self.ventana.mainloop() 
+
+    def creaCanvasPosturas(self):
+
+        self.canvasPosturas= Canvas(self.ventana,width=324,height=95)
+        self.canvasPosturas.grid(pady=(290,0 ),padx=(20,0))      
+        frame_posturas = Frame(self.canvasPosturas)
+        self.canvasPosturas.create_window((0, 0), window=frame_posturas, anchor='nw')
+        Label(text="Posturas",font=("arial",10),width=17,justify=LEFT).place(x=20,y=267)
+        Label(text="Placer",font=("arial",10),width=6,justify=LEFT).place(x=164,y=267)
+        Label(text="Agotamiento",font=("arial",10),width=9).place(x=220,y=267)
+        Label(text="Tiempo",font=("arial",10),width=5).place(x=300,y=267)
+        rows = 5
+        columns = 4
+        posturas =None
+        posturas = [[Entry() for j in range(columns)] for i in range(rows)]
+        list=['misionero','perrito','69','yunque','rueda']
+        listIntvar = []
+        for i in range(0, rows):
+                listIntvar.append(StringVar(value=list[i]))
+                posturas[i][0] = Entry(frame_posturas,font=("arial",10),textvariable=listIntvar[i], width=20,state="readonly")
+                posturas[i][0].grid(row=i, column=0, sticky='news')
+                posturas[i][1] = Entry(frame_posturas, width=8,justify=CENTER)
+                posturas[i][1].grid(row=i, column=1, sticky='news')
+                posturas[i][2] = Entry(frame_posturas, width=13,justify=CENTER)
+                posturas[i][2].grid(row=i, column=2, sticky='news')
+                posturas[i][3] = Entry(frame_posturas, width=8,justify=CENTER)
+                posturas[i][3].grid(row=i, column=3, sticky='news')
+        return posturas
+
+    def datosEntrada(self):
+
+        if self.canvasPosturas != None:
+            self.canvasPosturas.destroy()
+        self.posturas=self.creaCanvasPosturas()
+
+        self.minimizar = IntVar() 
+        Label (text="Desea :",bg="lightblue",fg="black",font=("arial",12)).place(x=20,y=60)
+        Radiobutton(text="Maximizar",bg="lightblue",font=("arial",10), variable=self.minimizar, value=0).place(x=90,y=60)
+        Radiobutton(text="Minimizar",bg="lightblue",fg="black",font=("arial",10), variable=self.minimizar, value=1).place(x=200,y=60)
+
+        Label(text="Criterio :",bg="lightblue",fg="black",font=("arial",12)).place(x=20,y=90)
+        self.criterio = IntVar(value=1)
+        Radiobutton(text="Tiempo",bg="lightblue",fg="black",font=("arial",10), variable=self.criterio, value='1').place(x=200,y=90)
+
+        self.umbral_org = IntVar()
+        Label(text="Umbral del orgasmo :",bg="lightblue",fg="black",font=("arial",12)).place(x=20,y=120)
+        Entry(textvariable=self.umbral_org,width=3).place(x=200,y=120)
+
+        self.energia_ini = IntVar()
+        Label(text="Energía inicial :",bg="lightblue",fg="black",font=("arial",12)).place(x=20,y=150)
+        Entry(textvariable=self.energia_ini,width=3).place(x=200,y=150)
+
+        self.placer_ini = IntVar()
+        Label(text="Placer inicial ",bg="lightblue",fg="black",font=("arial",12)).place(x=20,y=180)
+        Entry(textvariable=self.placer_ini,width=3).place(x=200,y=180)
+
+        Label (text="Restricciones :",bg="lightblue",fg="black",font=("arial",12)).place(x=20,y=210)
+        self.TodosVivos = IntVar(value=1) 
+        self.TodosOrgasmo = IntVar(value=1)
+        Checkbutton(text="Todos Vivos",bg="lightblue",fg="black",font=("arial",10), variable=self.TodosVivos, onvalue=1, offvalue=0).place(x=140,y=210)
+        Checkbutton(text="Todos Orgasmo",bg="lightblue",fg="black",font=("arial",10), variable=self.TodosOrgasmo, onvalue=1, offvalue=0).place(x=240,y=210)
+
+        Button (text='Ejecutar',font=("arial",10),width=10,height=1,command=self.modelo).place(x=130,y=430)      
+
+    def reiniciar(self):
+        self.umbral_org = None
+        self.energia_ini = None
+        self.placer_ini = None
+        if self.vr != None:
+            self.vr.destroy()    
+        self.datosEntrada()
+
+    def modelo(self):
+        Label(text="Espere    ...",bg="lightblue",fg="black",font=("arial",10),width=20,height=2).place(x=130,y=430)
+        listPosturas, listRestricciones, fo = self.prepararDatos()
+        resultado = EjecutarModelo(self, listPosturas, listRestricciones, fo)
+        self.muestraResultado(resultado)
+        muestraGrafica(listPosturas, resultado)
+
+    def prepararDatos(self):
+        #crear lista con los objetos postura
+        listPosturas = []
+        for i in range(len(self.posturas)):
+            listPosturas.append( Postura(self.posturas[i]) )
+        #poner las funciones objetivo en la lista fo
+        fo = [listPosturas[i].tiempo for i in range(len(listPosturas))]
+        #lista de restricciones seleccionadas
+        listRestricciones = []
+        if self.TodosVivos.get()==1: 
+            listRestricciones.append("TodosVivos") 
+        if self.TodosOrgasmo.get()==1: 
+            listRestricciones.append("TodosOrgasmo")  
+        return listPosturas, listRestricciones, fo
+
+    def muestraResultado(self,resultado):
+        self.vr = Toplevel(self.ventana)
+        self.vr.title("Resultado") 
+        self.vr.geometry("450x150"+"+"+str(420)+"+"+str(10)) 
+        self.vr.resizable(0,0)
+        self.vr.config(bg="blue", bd=12, relief="sunken")
+
+        listaresultado=  Listbox(self.vr, height=6, width=60, bg="black",fg="white",font=("arial",10))
+        listaresultado.place(x=0,y=0)
+
+        if resultado.x is None:
+            listaresultado.insert(END ,'RESULTADO INSATISFACIBLE')
+        else:
+            for i in range(len(resultado.x)):
+                listaresultado.insert(END ,f'El tiempo que hay que dedicarle a la postura {self.posturas[i][0].get()} es {round(resultado.x[i],3)}')
+        Button (self.ventana,text='Reiniciar',font=("arial",10),width=10,height=1,command=self.reiniciar).place(x=130,y=430)      
+
+def muestraGrafica(listaPosturas, resultado):
         fig, ax = plt.subplots()
-        p = [x.nombre for x in posturas]
-        plt.bar(p, resultado.x)
+        p = [x.placer for x in listaPosturas]
+        c = [x.agotamiento for x in listaPosturas]
+        plt.plot(resultado.x, p, label = 'Placer')
+        plt.plot(resultado.x, c, label = 'Agotamiento')
         ax.legend(loc = 'best')
         plt.show()
-# def main():
-#     s = read_script(minimizar, umbral_org, energia_ini)
-#     restr = crearRestricciones(s[3])
-#     a = modelo(s[0], s[1], s[2], restr, s[5])
-
-#     tiempo = a.x
-#     placer = [int(item.placer) for item in posturas]
-#     cansancio = [int(item.agotamiento) for item in posturas]
-
-#     fig, ax = plt.subplots()
-#     temperaturas = {'Placer':placer, 'Cansancio':cansancio}
-#     ax.plot(tiempo, placer, label = 'Placer')
-#     ax.plot(tiempo, cansancio, label = 'Cansancio')
-#     ax.legend(loc = 'best')
-#     plt.show()
-
-#     for i in range(len(a.x)):
-#         print(f'El {s[4]} que hay que dedicarle a la postura {posturas[i].name} es {a.x[i]}')
 
 if __name__ == '__main__':
-    main()
+    app=Raiz()
