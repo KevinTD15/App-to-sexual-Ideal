e3cca2ed25c6c18d67ab9b19df6dfeb162f734cd
KevinTD15 <talaveradiazkevin1999@gmail.com>
2022-12-11 10:04:07 -0500

    modificado para que funcione con el visual

--- posturas.py
+++ posturas.py
@@ -1,13 +1,10 @@
 class Postura: #en talla
-    def __init__(self,nombre):
-        self.nombre = nombre
-        self.placer = None
-        self.agotamiento = None 
+    def __init__(self,datos):
+        self.nombre = datos[0].get()
+        self.placer = datos[1].get()
+        self.agotamiento = datos[2].get()
+        self.tiempo = datos[3].get()
 
-p1 = Postura('misionero')
-p2 = Postura('perrito')
-p3 = Postura('69')
-p4 = Postura('yunque')
-p5 = Postura('rueda')
-
-posturas = [p1, p2, p3, p4, p5]
+        if self.placer == '':   self.placer=0
+        if self.agotamiento == '':   self.agotamiento=0
+        if self.tiempo == '':   self.tiempo=0
