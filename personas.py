d87bcccca31ba9c802b9277645259e0b3372849a
KevinTD15 <talaveradiazkevin1999@gmail.com>
2022-12-14 06:19:51 -0500

    clase persona creada

---
+++ personas.py
@@ -0,0 +1,11 @@
+class Persona: #en talla
+    def __init__(self,nombre):
+        self.nombre = nombre
+        self.placerInicial = 0
+        self.energiaInicial = 0
+        self.umbralOrgasmo = 0
+        self.posturas = []
+        '''
+        self.placerInicial = datos[1].get()
+        self.energiaInicial = datos[2].get()
+        self.umbralOrgasmo = datos[3].get()'''
