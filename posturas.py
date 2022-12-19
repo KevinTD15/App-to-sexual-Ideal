1b73b7e7ce069ff4e357d98947ef9308f43c3b7e
KevinTD15 <talaveradiazkevin1999@gmail.com>
2022-12-19 10:08:28 -0500

    annadida fo

--- posturas.py
+++ posturas.py
@@ -3,8 +3,6 @@ class Postura: #en talla
         self.nombre = datos[0].get()
         self.placer = datos[1].get()
         self.agotamiento = datos[2].get()
-        self.fo = datos[3].get()
 
         if self.placer == '':   self.placer=0
         if self.agotamiento == '':   self.agotamiento=0
-        if self.fo == '':   self.fo=0
